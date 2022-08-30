9dcbf32978b2341cc19f4e8331e4d0ae9b237459
Steve Fallet <steve.fallet@gmail.com>
2022-08-30 10:01:43 +0200

    1re solution

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,15 +1,26 @@
 /**
-* Jeu du nombre mystère
-* @author  Steve Fallet
-* @version 0.1
-* @since   2018-09-09 (date de création)
-*/
+ * Jeu du nombre mystère
+ * @author  Steve Fallet
+ * @version 1.0
+ * @since   2022-08-30 (date de création)
+ */
 
 //Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
 (function main() {
-    "use strict";
+    'use strict';
+
     /**
      * Retourne un nombre entier aléatoire compris entre min et max
+     * @param min
+     * @param max
+     * @returns {number}
+     */
+    // Constantes d'application
+    const NB_MIN = 1;
+    const NB_MAX = 100;
+
+    /**
+     * Fonction qui retourne un nombre aléatoire compris entre min et max
      * @param min
      * @param max
      * @returns {number}
@@ -18,11 +29,6 @@
         return Math.floor(Math.random() * (max - min) + min);
     }
 
-
-    // Constantes d'application
-    const NB_MIN = 1;
-    const NB_MAX = 100;
-
     // Récupération des éléments HTML à manipuler
     const strongNbMax = document.getElementById('nbMax');
     const strongNbMin = document.getElementById('nbMin');
@@ -30,29 +36,54 @@
     const txtNombre = document.getElementById('nombre');
     const ulHistorique = document.getElementById('historique');
 
-
-
+    // Tire le nombre mystère
     let nbMystere = tireNombre(NB_MIN, NB_MAX);
+    // Affiche le nombre dans la console pour faciliter la triche ;)
     console.log(nbMystere);
+    // Initialise le nombre d'essais
     let nbEssais = 0;
-    let reponse = null;
-    let message = 'Le nombre à deviner est compris entre 1 et 100.';
 
-    // Tant que la réponse est différente du nombre mystère
-    do {
+    // Remplace nombre min et max dans le HTML
+    strongNbMin.innerText = NB_MIN;
+    strongNbMax.innerText = NB_MAX;
+
+    // Ajoute une écoute sur l'envoi du formulaire (événement submit)
+    formulaire.addEventListener('submit', function (event) {
+
+        // Annule l'envoi du formulaire afin de rester sur la page
+        event.preventDefault();
+
         // Récupère la réponse et la converti en entier
-        reponse = parseInt(prompt(message));
+        const reponse = parseInt(txtNombre.value);
+
+        // Si l'utilisateur n'a pas entré un nombre valide
+        if (isNaN(reponse)) {
+            alert('Entrez un nombre valide !');
+            return; // On s'arrête ici
+        }
 
         // Comptabilise l'essai
         nbEssais += 1;
 
-        // Définit le message a afficher au prochain tour
+        // Teste la réponse de l'utilisateur
         if (reponse > nbMystere) {
-            message = "C'est moins";
+            ulHistorique.innerHTML += `<li>C'est moins que <strong>${reponse}</strong> !</li>`;
+        } else if (reponse < nbMystere) {
+            ulHistorique.innerHTML += `<li>C'est plus que <strong>${reponse}</strong> !</li>`;
         } else {
-            message = "C'est plus";
+            ulHistorique.innerHTML +=
+                `<li>
+                    <div>C'est bien <strong>${reponse} !</strong></div>
+                    <div>
+                        Bravo, tu as gagné en 
+                        <strong>${nbEssais}</strong> coups.
+                    </div>
+                </li>`;
         }
-    } while (reponse !== nbMystere);
 
-    alert("Bravo, tu as gagné en " + nbEssais + " coups !");
+        // Vide le champ du formulaire
+        txtNombre.value = '';
+        // Donne le focus au champ du formulaire
+        txtNombre.focus();
+    });
 }()); //main IIFE
