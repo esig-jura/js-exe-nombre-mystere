9bec1471218308b4d03bef3473d70c946fda00f4
Steve Fallet <steve.fallet@divtec.ch>
2023-02-08 09:52:33 +0100

    suppression IIFE

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,22 +1,20 @@
 /**
-* Jeu du nombre mystère
-* @author  Steve Fallet
-* @version 0.1
-* @since   2018-09-09 (date de création)
-*/
+ * Jeu du nombre mystère
+ * @author  Steve Fallet
+ * @version 2.0
+ * @since   2018-09-09 (date de création)
+ */
 
-// Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
-(function main() {
-    "use strict";
+'use strict';
 
-    /**
-     * Retourne un nombre entier aléatoire compris entre min et max
-     * @param min
-     * @param max
-     * @returns {number}
-     */
-    function tireNombre(min, max) {
-        return Math.floor(Math.random() * (max - min) + min);
-    }
+/**
+ * Retourne un nombre entier aléatoire compris entre min et max
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+function tireNombre(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
-}()); // main IIFE
+alert(tireNombre(50,150));
